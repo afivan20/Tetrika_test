9330bf78401fed48c471db897f309705832caacc
Ivan <afivan@bk.ru>
2022-07-13 16:54:52 +0300

    improved parser

--- tasks/2.wiki_parser.py
+++ tasks/2.wiki_parser.py
@@ -7,30 +7,35 @@ LETTERS = 'АБВГДЕЁЖЗИӢКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
 ANIMALS_ALPHABET = dict(zip(LETTERS, [0 for n in range(1, 34)]))
 
 def parse(link):
-    response = requests.get(link)
-    text = response.text
-    tree = html.document_fromstring(text)
-
-    names = tree.xpath('//div[contains(@class, "mw-category mw-category-columns")]//text()')
-    for name in names:
-        if name[0] in ANIMALS_ALPHABET:
-            ANIMALS_ALPHABET[name[0]] += 1
-        if name[0] == 'A':  # latin 'A'
-            return
-
     try:
-        href = tree.xpath('//a[contains(text(), "Следующая")]/@href')[1]
-        domain_name = urlparse(response.url).netloc
-        next_page = f'http://{domain_name}{href}'
-        parse(next_page)
-    except Exception:
+        response = requests.get(link)
+        text = response.text
+        tree = html.document_fromstring(text)
+        names = tree.xpath('//div[contains(@class, "mw-category mw-category-columns")]//text()')
+        
+    except Exception as error:
+        print(f'Упс... Что-то пошло не так: {error}')
         return
+    
+    if len(names) != 0:
+        for name in names:
+            if name[0] in ANIMALS_ALPHABET:
+                ANIMALS_ALPHABET[name[0]] += 1
+            if name[0] == 'A':  # latin 'A'
+                return
+
+    href = tree.xpath('//a[contains(text(), "Следующая")]/@href')
+    if len(href) != 0:
+        domain_name = urlparse(response.url).netloc
+        next_page = f'http://{domain_name}{href[0]}'
+        parse(next_page)
+    return 
 
 
 if __name__ == '__main__':
-    print('\nИдет парсинг Wiki, мы собираем десятки тысяч названий животных.\nПроцесс займет не более 1 минуты. Пожалуйста подождите...')
+    print('\nЗапущен процесс парсинга Wiki, мы собираем десятки тысяч названий животных.\nПроцесс займет около 1 минуты. Пожалуйста подождите...\n')
     url = 'https://inlnk.ru/jElywR'
     parse(url)
-    print('\nКоличество животных на каждую букву алфавита:')
+    print('Количество животных на каждую букву алфавита:')
     for letter in ANIMALS_ALPHABET:
         print(f'{letter}: {ANIMALS_ALPHABET[letter]}')
---
+++ test.py
@@ -0,0 +1,3 @@
+l = []
+
+print(l is not None)
